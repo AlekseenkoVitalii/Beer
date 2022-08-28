219cf396b0831759d7f394e0d24abc5b338626bf
Vitalii Alieksieienko <Alieksieienkovitalii@gmail.com>
2022-08-28 16:33:19 +0300

    Code refactoring

--- src/Form/BeerType.php
+++ src/Form/BeerType.php
@@ -7,6 +7,7 @@ use App\Entity\Supplier;
 use App\Entity\TypeBeer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -16,21 +17,19 @@ class BeerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('supplier', EntityType::class,
-                [
+            ->add('name', TextType::class, [
+                'constraints' => [new NotBlank()]])
+            ->add('description', TextType::class, [
+                'constraints' => [new NotBlank()]])
+            ->add('supplier', EntityType::class, [
                     'class' => Supplier::class,
                     'choice_name' => 'id',
                     'multiple' => true,
-                    'constraints' => [new NotBlank()]
-                ])
-            ->add('type', EntityType::class,
-                [
+                    'constraints' => [new NotBlank()]])
+            ->add('type', EntityType::class, [
                     'class' => TypeBeer::class,
                     'choice_name' => 'id',
-                    'constraints' => [new NotBlank()]
-                ]);
+                    'constraints' => [new NotBlank()]]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
